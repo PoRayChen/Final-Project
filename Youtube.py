67e88ff7a0914f802b3b8847997ca0252c52b114
PoRayChen <57026449+PoRayChen@users.noreply.github.com>
2021-05-02 00:49:07 -0400

    Final

--- Youtube.py
+++ Youtube.py
@@ -9,14 +9,36 @@ Folder_Name = ""
 def openLocation():
     global Folder_Name
     Folder_Name = filedialog.askdirectory()
-    if(len(Folder_Name)  > 1):
+    if len(Folder_Name) > 1:
         locationError.config(text=Folder_Name, fg="blue")
 
     else:
         locationError.config(text="Choose Folder", fg ="red")
 
-def DownloadVideo ():
-    choice = ytdchoices.get
+def DownloadVideo():
+    choice = YTDChoose.get
+    url = ytdEntry.get()
+
+    if(len(url)>1):
+        YTDError.config(text="")
+        yt = YouTube(url)
+
+        if (choice == choices [0]):
+            select = yt.streams.filter(progressive=True).first()
+
+        elif (choice == choices [1]):
+            select = yt.streams.filter(progressive=True, file_extension='mp4').last()
+        elif (choice == choices [2]):
+            select = yt.streams.filter(only_audio=True).first()
+        else:
+            YTDError.config ("Paste Link", fg ="red")
+    select.download(Folder_Name)
+    YTDError.config(text="Download Finished")
+
+
+
+
+
 #gui for Downloader
 root = Tk()
 root.title("YouTube Downloader")
@@ -41,7 +63,7 @@ saveLabel = Label(root, text="Save Video", font=("jost", 16))
 saveLabel.grid()
 
 #save entry
-saveEntry = Button(root, width=16, bg="pink", fg="black", text="Choose")
+saveEntry = Button(root, width=16, bg="pink", fg="black", text="Choose", command =openLocation)
 saveEntry.grid()
 
 #error msg location
@@ -58,7 +80,7 @@ YTDChoose = ttk.Combobox(root, values=choices)
 YTDChoose.grid()
 
 #download
-downloadbtn = Button(root, text="Download", width= 12, fg="black")
+downloadbtn = Button(root, text="Download", width= 12, fg="black", command= DownloadVideo)
 downloadbtn.grid()
 
 root.mainloop()
